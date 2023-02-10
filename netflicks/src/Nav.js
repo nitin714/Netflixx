3b26adb9a6f5314d94e07fb08c96279f204c62be
Nitin Vadhiya <nitinvadhiya123@gmail.com>
2023-02-10 16:37:23 +0530

    changes pushing

---
+++ netflicks/src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/564x/ce/1a/68/ce1a68b494cd4914a8e724d40645ac6c.jpg"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
